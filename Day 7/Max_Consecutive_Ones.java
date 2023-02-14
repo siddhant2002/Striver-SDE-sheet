2e7ffd0b3278769be4e53a5b8918d56e89bfb884
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-14 13:44:46 +0530

    Day 7 updated

---
+++ Day 7/Max_Consecutive_Ones.java
@@ -0,0 +1,22 @@
+package Day 5;
+
+class Max_Consecutive_Ones 
+{
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int p=0,m=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] == 1)
+            {
+                m++;
+            }
+            else
+            {
+                p=Math.max(p,m);
+                m=0;
+            }
+        }
+        p=Math.max(p,m);
+        return p;
+    }
+}
